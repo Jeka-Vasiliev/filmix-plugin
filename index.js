a9ccf49902f6366b894be902e385229232141f61
Jeka-Vasiliev <zheka_v@inbox.ru>
2017-05-25 09:15:41 +0300

    Stub for chome extention

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+
+function changeCurrentUrl (newUrl) {
+  chrome.tabs.query({ currentWindow: true, active: true }, (tab) => {
+    chrome.tabs.update(tab.id, { url: newUrl })
+  })
+}
+
+function setButtonHandler () {
+  const button = document.getElementById('goto-button')
+  button.addEventListener('click', () => changeCurrentUrl('https://www.google.ru/'))
+}
+
+function main () {
+  setButtonHandler()
+}
+
+document.addEventListener('DOMContentLoaded', main)
