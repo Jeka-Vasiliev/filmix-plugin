0f18b74fa550441369b0bb792200abea3f67a99c
Jeka Vasiliev <zheka_v@inbox.ru>
2018-03-09 20:22:11 +0300

    append required params

--- src/inject/api.ts
+++ src/inject/api.ts
@@ -31,5 +31,20 @@ function buildSearchRequestBody(pageNumber: number, genreIds: string[]) {
   const body = new URLSearchParams();
   body.append('search_start', pageNumber.toString());
   genreIds.forEach(id => body.append('ganre[]', id));
+
+  body.append('scf', 'fx');
+  body.append('story', '');
+  body.append('do', 'search');
+  body.append('subaction', 'search');
+  body.append('years_ot', '1902');
+  body.append('years_do', '2018');
+  body.append('kpi_ot', '1');
+  body.append('kpi_do', '10');
+  body.append('imdb_ot', '1');
+  body.append('imdb_do', '10');
+  body.append('sort_name', '');
+  body.append('undefined', 'asc');
+  body.append('sort_date', '');
+  body.append('sort_favorite', '');
   return body.toString();
 }
