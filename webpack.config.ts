8c389a1267e9cb5755b965f844f9b5c0751865b7
Jeka Vasiliev <zheka_v@inbox.ru>
2019-07-07 15:55:08 +0300

    update all dependensies

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,44 @@
-import CleanWebpackPlugin from 'clean-webpack-plugin'
-import CopyWebpackPlugin from 'copy-webpack-plugin'
-import path from 'path'
-import { Configuration } from 'webpack'
-import ZipPlugin from 'zip-webpack-plugin'
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import path from "path";
+import { Configuration } from "webpack";
+import ZipPlugin from "zip-webpack-plugin";
 
-interface Env { [key: string]: string }
-interface Argv { mode: Configuration['mode'] }
+interface Env {
+  [key: string]: string;
+}
+interface Argv {
+  mode: Configuration["mode"];
+}
 
 export default (_: Env, { mode }: Argv): Configuration => ({
   entry: {
-    background: path.resolve('./src/background'),
-    inject: path.resolve('./src/inject'),
-    options: path.resolve('./src/options'),
+    background: path.resolve("./src/background"),
+    inject: path.resolve("./src/inject"),
+    options: path.resolve("./src/options")
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve('./dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve("./dist")
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: ['babel-loader'], exclude: /node_modules/ },
-    ],
+      { test: /\.tsx?$/, loader: ["babel-loader"], exclude: /node_modules/ }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx'],
+    extensions: [".ts", ".tsx"]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([{ from: 'src/manifest.json' }, { from: 'src/options.html' }]),
-    ...mode === 'production' ? [new ZipPlugin({filename: 'filmix-random-worshipper'})] : [],
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: "src/manifest.json" },
+      { from: "src/options.html" }
+    ]),
+    ...(mode === "production"
+      ? [new ZipPlugin({ filename: "filmix-random-worshipper" })]
+      : [])
   ],
   mode,
-  devtool: mode === 'development' ? 'source-map' : false,
-})
+  devtool: mode === "development" ? "source-map" : false
+});
